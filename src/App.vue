e0c11d2fc886e10bde03d3b84182371d896522eb
zhangyichi <917735466@qq.com>
2021-05-22 22:01:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- the original content -->
+  <!--
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/SingleSkillTree.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "SingleSkillTree"
+}
+</script>
+
+<style scoped>
+
+</style>
