70f3488f82fe3f6fdbac10782af9cbb6a420856f
zhangyichi <917735466@qq.com>
2021-05-23 20:20:27 +0800

    ready

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png"/>
-  <hello-world msg="Hello Vue 3 + Vite"/>-->
-  <single-skill-tree></single-skill-tree>
+  <!--
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <hello-world msg="Hello Vue 3 + Vite"/>
+  -->
+  <single-skill-tree style="width: 90%;margin-left: 5%;margin-right: 5%"
+                     :skill-tree="allSkillTrees['troy_adi_archer_teucer']"/>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import SingleSkillTree from "./components/SingleSkillTree.vue";
+import data from "./assets/data.json";
 
 export default {
   name: 'App',
   components: {
     'hello-world': HelloWorld,
     'single-skill-tree': SingleSkillTree
+  },
+  data() {
+    return {
+      allSkillTrees: data
+    }
   }
 }
 </script>
