7ad3e7fe683b284fb13ca58251d86e1fbe5ed368
zhangyichi <917735466@qq.com>
2021-06-17 22:35:53 +0800

    base

--- src/components/UnitChangePanel.vue
+++ src/components/UnitChangePanel.vue
@@ -1,36 +1,36 @@
 <template>
   <div style="text-align: left">
-    <div>单位：{{ current.nameZhCn }}</div>
-    <div>HP：{{ changedValue(original.singleHp, current.singleHp) }}</div>
-    <div>护甲：{{ changedValue(original.armour, current.armour) }}</div>
-    <div>盾挡：{{ changedValue(original.missileBlockChance, current.missileBlockChance) }}</div>
-    <div>士气：{{ changedValue(original.morale, current.morale) }}</div>
-    <div>速度：{{ changedValue(original.speed, current.speed) }}</div>
-    <div>近攻：{{ changedValue(original.meleeAttack, current.meleeAttack) }}</div>
-    <div>近防：{{ changedValue(original.meleeDefence, current.meleeDefence) }}</div>
-    <div>冲锋：{{ changedValue(original.chargeBonus, current.chargeBonus) }}</div>
-    <template v-if="original.primaryMeleeWeapon!==current.primaryMeleeWeapon">
-      <div>近战基础杀伤：{{ changedValue(original.meleeDamage, current.meleeDamage) }}</div>
-      <div>近战破甲杀伤：{{ changedValue(original.meleeApDamage, current.meleeApDamage) }}</div>
-      <div>近战攻击间隔：{{ changedValue(original.meleeAttackInterval, current.meleeAttackInterval) }}</div>
-      <div>近战对英雄加成：{{ changedValue(original.bonusVsHeroes, current.bonusVsHeroes) }}</div>
+    <div>单位：{{ current['nameZhCn'] }}</div>
+    <div>HP：{{ changedValue(original['singleHp'], current['singleHp']) }}</div>
+    <div>护甲：{{ changedValue(original['armour'], current['armour']) }}</div>
+    <div>盾挡：{{ changedValue(original['missileBlockChance'], current['missileBlockChance']) }}</div>
+    <div>士气：{{ changedValue(original['morale'], current['morale']) }}</div>
+    <div>速度：{{ changedValue(original['speed'], current['speed']) }}</div>
+    <div>近攻：{{ changedValue(original['meleeAttack'], current['meleeAttack']) }}</div>
+    <div>近防：{{ changedValue(original['meleeDefence'], current['meleeDefence']) }}</div>
+    <div>冲锋：{{ changedValue(original['chargeBonus'], current['chargeBonus']) }}</div>
+    <template v-if="original['primaryMeleeWeapon']!==current['primaryMeleeWeapon']">
+      <div>近战基础杀伤：{{ changedValue(original['meleeDamage'], current['meleeDamage']) }}</div>
+      <div>近战破甲杀伤：{{ changedValue(original['meleeApDamage'], current['meleeApDamage']) }}</div>
+      <div>近战攻击间隔：{{ changedValue(original['meleeAttackInterval'], current['meleeAttackInterval']) }}</div>
+      <div>近战对英雄加成：{{ changedValue(original['bonusVsHeroes'], current['bonusVsHeroes']) }}</div>
     </template>
     <template v-else>
       <div>近战武器数据无变化</div>
     </template>
     <template
-        v-if="current.primaryMissileWeapon!==original.primaryMissileWeapon
-        ||current.missileAttackInterval!==original.missileAttackInterval">
-      <div>远程基础杀伤：{{ changedValue(original.missileDamage, current.missileDamage) }}</div>
-      <div>远程破甲杀伤：{{ changedValue(original.missileApDamage, current.missileApDamage) }}</div>
-      <div>远程攻击间隔：{{ changedValue(original.missileAttackInterval, current.missileAttackInterval) }}</div>
+        v-if="current['primaryMissileWeapon']!==original['primaryMissileWeapon']
+        ||current['missileAttackInterval']!==original['missileAttackInterval']">
+      <div>远程基础杀伤：{{ changedValue(original['missileDamage'], current['missileDamage']) }}</div>
+      <div>远程破甲杀伤：{{ changedValue(original['missileApDamage'], current['missileApDamage']) }}</div>
+      <div>远程攻击间隔：{{ changedValue(original['missileAttackInterval'], current['missileAttackInterval']) }}</div>
       <div>射程：{{ changedValue(original.range, current.range) }}</div>
     </template>
     <template v-else>
       <div>远程武器数据无变化</div>
     </template>
     <div>
-      弹药数量：{{ changedValue(original.primaryAmmo, current.primaryAmmo) }}
+      弹药数量：{{ changedValue(original['primaryAmmo'], current['primaryAmmo']) }}
     </div>
     <div v-html="'地形数据效果组：'+changedGround"></div>
     <div v-html="'属性：'+changedAttribute"></div>
@@ -53,55 +53,7 @@ export default {
   },
   data() {
     return {
-      heroDetail: hero,
-      original: {
-        nameZhCn: '原名称',
-        singleHp: 10,
-        armour: 25,
-        missileBlockChance: 20,
-        morale: 50,
-        speed: 50,
-        meleeAttack: 40,
-        meleeDefence: 40,
-        chargeBonus: 20,
-        primaryMeleeWeapon: 'test melee',
-        meleeDamage: 200,
-        meleeApDamage: 150,
-        meleeAttackInterval: 4.0,
-        bonusVsHeroes: 20,
-        primaryMissileWeapon: 'test missile',
-        missileDamage: 200,
-        missileApDamage: 150,
-        missileAttackInterval: 4.0,
-        primaryAmmo: 30,
-        range: 180,
-        groundStatEffectGroup: '重装步兵',
-        attributes: '攻城者'
-      },
-      current: {
-        nameZhCn: '当前名称',
-        singleHp: 20,
-        armour: 30,
-        missileBlockChance: 40,
-        morale: 60,
-        speed: 90,
-        meleeAttack: 40,
-        meleeDefence: 40,
-        chargeBonus: 20,
-        primaryMeleeWeapon: 'test melee2',
-        meleeDamage: 200,
-        meleeApDamage: 150,
-        meleeAttackInterval: 4.0,
-        bonusVsHeroes: 20,
-        primaryMissileWeapon: 'test missile 2',
-        missileDamage: 200,
-        missileApDamage: 150,
-        missileAttackInterval: 4.0,
-        primaryAmmo: 30,
-        range: 180,
-        groundStatEffectGroup: '大型部队',
-        attributes: '攻城者|心理免疫'
-      }
+      heroDetail: hero
     }
 
   },
@@ -118,9 +70,20 @@ export default {
     },
   },
   computed: {
+    original() {
+      let nameParts = this.providedBodyguardUnit.split('_chariot');
+      if (nameParts.length < 2) {
+        nameParts = this.providedBodyguardUnit.split('_horse');
+      }
+      let originalUnit = nameParts[0];
+      return this.heroDetail[originalUnit];
+    },
+    current() {
+      return this.heroDetail[this.providedBodyguardUnit];
+    },
     changedGround() {
-      let oriGroundArr = this.original.groundStatEffectGroup.split("|");
-      let currGroundArr = this.current.groundStatEffectGroup.split("|");
+      let oriGroundArr = this.original['groundStatEffectGroup'].split("|");
+      let currGroundArr = this.current['groundStatEffectGroup'].split("|");
       let less = [];
       let more = [];
       const _oriGroundSet = new Set(oriGroundArr);
@@ -178,15 +141,6 @@ export default {
         return lessStr + "<br>&emsp;&emsp;&emsp;" + moreStr
       }
     }
-  },
-  mounted() {
-    this.current = this.heroDetail[this.providedBodyguardUnit];
-    let nameParts = this.providedBodyguardUnit.split('_chariot');
-    if (nameParts.length < 2) {
-      nameParts = this.providedBodyguardUnit.split('_horse');
-    }
-    let originalUnit = nameParts[0];
-    this.original = this.heroDetail[originalUnit];
   }
 }
 </script>
